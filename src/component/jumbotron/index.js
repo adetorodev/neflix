8999fe315bd119b4b4610fdbee771dc2650b129f
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-12-22 03:50:15 +0100

    task: take a break, start jumbotron

---
+++ src/component/jumbotron/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Inner } from './Jumbotron'
+
+export default function Jumbotron(){
+    return (
+        <Inner>
+            <h1>Hello</h1>
+        </Inner>
+    )
+}
