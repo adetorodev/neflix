04c785869c4b2626b1edbde646039f46bb0766a3
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-01-17 14:21:30 +0100

    connect with firebase and seed data

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,4 +1,18 @@
-import Firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
+import Firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+// import { seedDatabase } from "../seed";
 
+const config = {
+  apiKey: "AIzaSyALK_BWORRN5f-kQbeTpKmDP8qhn1VSlVE",
+  authDomain: "netflix-cb976.firebaseapp.com",
+  projectId: "netflix-cb976",
+  storageBucket: "netflix-cb976.appspot.com",
+  messagingSenderId: "254703508901",
+  appId: "1:254703508901:web:90ca29c383874e029a1c4c",
+};
+const firebase = Firebase.initializeApp(config);
+
+// seedDatabase(firebase);
+
+export { firebase };
