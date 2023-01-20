605e8b084a54a8cf54d61921e71886933430f595
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-01-20 01:15:29 +0100

    firebase connected

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import 'normalize.css'
-import { GlobalStyles } from './global-styles'
+import "normalize.css";
+import { GlobalStyles } from "./global-styles";
 import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from './context/firebase'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-  <GlobalStyles />
-  <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </>
-    
 );
